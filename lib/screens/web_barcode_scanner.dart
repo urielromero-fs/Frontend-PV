e22f4db0e36f8d65215ca9654de35342326c7e14
BrianHSL-256 <briansorianoleal@gmail.com>
2026-02-23 13:20:05 -0600

    Scanner fixed

--- lib/screens/web_barcode_scanner.dart
+++ lib/screens/web_barcode_scanner.dart
@@ -1,61 +1,4 @@
-// import 'package:flutter/material.dart';
-// import 'package:mobile_scanner/mobile_scanner.dart';
 
-// class WebBarcodeScanner extends StatefulWidget {
-//   final Function(String) onScanned;
-
-//   const WebBarcodeScanner({super.key, required this.onScanned});
-
-//   @override
-//   State<WebBarcodeScanner> createState() => _WebBarcodeScannerState();
-// }
-
-// class _WebBarcodeScannerState extends State<WebBarcodeScanner> {
-//   // Usamos el mismo controlador que en móvil
-//   late MobileScannerController controller;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     controller = MobileScannerController(
-//       facing: CameraFacing.back,
-//       detectionSpeed: DetectionSpeed.normal, // Evita procesar demasiados frames por segundo
-//     );
-//   }
-
-//   @override
-//   void dispose() {
-//     controller.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       color: Colors.black,
-//       child: MobileScanner(
-//         controller: controller,
-//         scanWindow: Rect.fromCenter(
-//           center: Offset(MediaQuery.of(context).size.width / 2, 175), // Ajustado a tu modal de 350
-//           width: 200,
-//           height: 200,
-//         ),
-
-//         onDetect: (capture) {
-//           final List<Barcode> barcodes = capture.barcodes;
-//           if (barcodes.isNotEmpty) {
-//             final String? code = barcodes.first.rawValue;
-//             if (code != null) {
-//               // Detenemos el scanner un momento para evitar múltiples pops
-//               controller.stop(); 
-//               widget.onScanned(code);
-//             }
-//           }
-//         },
-//       ),
-//     );
-//   }
-// }
 
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
@@ -80,7 +23,7 @@ class _WebBarcodeScannerState extends State<WebBarcodeScanner> {
     super.initState();
     controller = MobileScannerController(
       facing: CameraFacing.back,
-      
+      cameraResolution: const Size(1920, 1080),
       detectionSpeed: DetectionSpeed.normal,
       
       
